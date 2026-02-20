b2804d0b6dd168ee132ef016b030de5011415ced
George Dinu <georgedinu2210@gmail.com>
2026-02-20 08:55:35 +0200

    Delete FLoating Lines

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { WebVitals } from '../components/web-vitals';
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +15,6 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        <WebVitals/>
         {children}
       </body>
     </html>
--- components/HeroSection/heroSection.jsx
+++ components/HeroSection/heroSection.jsx
@@ -1,13 +1,9 @@
 import './styles.css';
 import ButtonContact from '../ButtonContact/ButtonContact';
-import FloatingLinesClient from './FloatingLinesClient';
 
 export default function HeroSection(){
     return(
         <div className="heroSection">
-            <div className="aurora">
-                <FloatingLinesClient/>
-            </div>
             <div className="contentHero">
                 <h1 className="titleHero">Website-uri cu <span>impact</span> pe vânzări si <strong>Vizibilitate </strong>in Online</h1>
                 <p className="descriereHero">Site-uri de prezentare și aplicații web, dezvoltate de la zero, special pentru tine și afacerea ta.</p>
